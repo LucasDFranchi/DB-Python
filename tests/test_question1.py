21ddbfe1b26b50d22b90a69d58277a50bea82a1c
Lucas Dalla Corte Franchi <lucas.franchi@dbserver.com.br>
2024-05-05 20:08:16 -0300

    test: add test to handle exceptions

--- tests/test_question1.py
+++ tests/test_question1.py
@@ -4,7 +4,6 @@ from src import Contract, Contracts
 
 class TestContracts(unittest.TestCase):
     def test_get_top_N_open_contracts(self):
-        #teste com dados de entrada do exemplo
         contracts = [
             Contract(1, 1), 
             Contract(2, 2),
@@ -15,16 +14,27 @@ class TestContracts(unittest.TestCase):
         renegotiated = [3]  
         top_n = 3           
 
-        #resultado esperado
         expected_open_contracts = [5, 4, 2]  
 
-        #chamando a função à ser testada com os dados informados acima
         contracts_obj = Contracts()
         actual_open_contracts = contracts_obj.get_top_N_open_contracts(contracts, renegotiated, top_n)
 
-        #confere se o retorno esperado é o mesmo retorno da função
         self.assertEqual(expected_open_contracts, actual_open_contracts)
 
-#executa o teste
+    def test_get_top_N_open_contracts_exception(self):
+        contracts = [
+            Contract(1, 1), 
+            Contract(2, 2),
+            Contract(3, 3),
+            Contract(4, 4),
+            Contract(5, 5)
+        ]
+        renegotiated = [3]  
+        top_n = 0
+
+        contracts_obj = Contracts()
+        with self.assertRaises(ValueError):
+            actual_open_contracts = contracts_obj.get_top_N_open_contracts(contracts, renegotiated, top_n)
+
 if __name__ == '__main__':
     unittest.main()
