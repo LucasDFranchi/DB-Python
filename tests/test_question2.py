f9dcd1309d17f7ec0680d31dfa7ab30e4fdf49c6
Lucas Dalla Corte Franchi <lucas.franchi@dbserver.com.br>
2024-05-05 20:00:18 -0300

    tests: break tests and add more cases

--- tests/test_question2.py
+++ tests/test_question2.py
@@ -3,24 +3,27 @@ import unittest
 from src import Orders
 
 class TestOrders(unittest.TestCase):
-    #prepara ambiente de teste, executa a cada teste feito
+
     def setUp(self):
         self.ordem = Orders()
 
     def test_combine_orders(self):
-
-        #teste com uma lista pequena, com dados de entrada do exemplo
         requests_1 = [70, 10, 30]
         n_max_1 = 100
-        #chamando a função à ser testada com os dados informados acima, informando o resultado esperado
+
         self.assertEqual(self.ordem.combine_orders(requests_1, n_max_1), 2)
 
-        #teste com uma lista onde todas as viagens são únicas
+    def test_combine_orders_again(self):
         requests_2 = [100, 90, 95]
         n_max_2 = 100
-        #chamando a função à ser testada com os dados informados acima, informando o resultado esperado
+        
         self.assertEqual(self.ordem.combine_orders(requests_2, n_max_2), 3)
 
-#executa o teste
+    def test_combine_order_negative(self):
+        requests = [100, 90, 95]
+        n_max = 0
+        with self.assertRaises(Exception):
+            self.ordem.combine_orders(requests, n_max)
+
 if __name__ == '__main__':
     unittest.main()
